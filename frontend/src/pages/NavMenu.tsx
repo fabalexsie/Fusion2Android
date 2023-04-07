3853c833975b4733f8722ae685ccf7b74a240ebe
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-04-07 23:07:27 +0200

    extract NavMenu

---
+++ frontend/src/pages/NavMenu.tsx
@@ -0,0 +1,83 @@
+import { Navbar, Title, createStyles, rem } from '@mantine/core';
+import { Link, useLocation } from 'react-router-dom';
+import { getLastOpenedProjects } from '../util/localStorageUtil';
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    boxSizing: 'border-box',
+    display: 'block',
+    textDecoration: 'none',
+    borderTopRightRadius: theme.radius.md,
+    borderBottomRightRadius: theme.radius.md,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    padding: `0 ${theme.spacing.md}`,
+    fontSize: theme.fontSizes.sm,
+    marginRight: theme.spacing.md,
+    fontWeight: 500,
+    height: rem(44),
+    lineHeight: rem(44),
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[5]
+          : theme.colors.gray[1],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      borderLeftColor: theme.fn.variant({
+        variant: 'filled',
+        color: theme.primaryColor,
+      }).background,
+      backgroundColor: theme.fn.variant({
+        variant: 'filled',
+        color: theme.primaryColor,
+      }).background,
+      color: theme.white,
+    },
+  },
+}));
+
+export function NavMenu({ opened }: { opened: boolean }) {
+  const { classes, cx } = useStyles();
+  const { pathname } = useLocation();
+
+  const lastOpenedProjects = getLastOpenedProjects();
+
+  return (
+    <Navbar
+      p="md"
+      hidden={!opened}
+      hiddenBreakpoint="sm"
+      width={{ sm: 200, lg: 300 }}
+    >
+      <Navbar.Section>
+        <Title order={4}>Last visited projects</Title>
+      </Navbar.Section>
+      <Navbar.Section grow mt="md">
+        {lastOpenedProjects.map((proj, i) => (
+          <Link
+            key={proj.projectId}
+            to={`/${proj.projectId}`}
+            className={cx(classes.link, {
+              [classes.linkActive]: `/${proj.projectId}` === pathname,
+            })}
+          >
+            Project {proj.name}
+          </Link>
+        ))}
+        {/* Links sections */}
+      </Navbar.Section>
+      <Navbar.Section>{/* Footer with user */}</Navbar.Section>
+    </Navbar>
+  );
+}
--- frontend/src/pages/RootPage.tsx
+++ frontend/src/pages/RootPage.tsx
@@ -4,67 +4,14 @@ import {
   Header,
   Image,
   MediaQuery,
-  Navbar,
   Title,
-  createStyles,
-  rem,
 } from '@mantine/core';
 import { useState } from 'react';
-import { Link, Outlet, useLocation } from 'react-router-dom';
-import { getLastOpenedProjects } from '../util/localStorageUtil';
-
-const useStyles = createStyles((theme) => ({
-  link: {
-    boxSizing: 'border-box',
-    display: 'block',
-    textDecoration: 'none',
-    borderTopRightRadius: theme.radius.md,
-    borderBottomRightRadius: theme.radius.md,
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-    padding: `0 ${theme.spacing.md}`,
-    fontSize: theme.fontSizes.sm,
-    marginRight: theme.spacing.md,
-    fontWeight: 500,
-    height: rem(44),
-    lineHeight: rem(44),
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-
-    '&:hover': {
-      backgroundColor:
-        theme.colorScheme === 'dark'
-          ? theme.colors.dark[5]
-          : theme.colors.gray[1],
-      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    },
-  },
-
-  linkActive: {
-    '&, &:hover': {
-      borderLeftColor: theme.fn.variant({
-        variant: 'filled',
-        color: theme.primaryColor,
-      }).background,
-      backgroundColor: theme.fn.variant({
-        variant: 'filled',
-        color: theme.primaryColor,
-      }).background,
-      color: theme.white,
-    },
-  },
-}));
+import { Link, Outlet } from 'react-router-dom';
+import { NavMenu } from './NavMenu';
 
 export function RootPage() {
-  const { classes, cx } = useStyles();
-  const { pathname } = useLocation();
-
   const [opened, setOpened] = useState(false);
-
-  const lastOpenedProjects = getLastOpenedProjects();
 
   return (
     <AppShell
@@ -98,33 +45,7 @@ export function RootPage() {
         </Header>
       }
       navbarOffsetBreakpoint="sm"
-      navbar={
-        <Navbar
-          p="md"
-          hidden={!opened}
-          hiddenBreakpoint="sm"
-          width={{ sm: 200, lg: 300 }}
-        >
-          <Navbar.Section>
-            <Title order={4}>Last visited projects</Title>
-          </Navbar.Section>
-          <Navbar.Section grow mt="md">
-            {lastOpenedProjects.map((proj, i) => (
-              <Link
-                key={proj.projectId}
-                to={`/${proj.projectId}`}
-                className={cx(classes.link, {
-                  [classes.linkActive]: `/${proj.projectId}` === pathname,
-                })}
-              >
-                Project {proj.name}
-              </Link>
-            ))}
-            {/* Links sections */}
-          </Navbar.Section>
-          <Navbar.Section>{/* Footer with user */}</Navbar.Section>
-        </Navbar>
-      }
+      navbar={<NavMenu opened={opened} />}
     >
       <Outlet></Outlet>
     </AppShell>
